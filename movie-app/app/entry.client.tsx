ce0c7293321b454c1469a998daeeba784acba1e0
Jessica Castro <jessica.castros@outlook.com>
2025-04-30 21:42:29 -0300

    chore(movie-app): add nvmrc, node and browser setup for msw and registered service worker

--- movie-app/app/entry.client.tsx
+++ movie-app/app/entry.client.tsx
@@ -2,6 +2,22 @@ import { RemixBrowser } from '@remix-run/react'
 import { startTransition } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
-startTransition(() => {
-  hydrateRoot(document, <RemixBrowser />)
+async function enableAPIMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return 
+  }
+
+    const { browserWorker } = await import('./mocks/browser')
+
+    await browserWorker.start()
+  
+}
+
+enableAPIMocking().then(() => {
+  startTransition(() => {
+    hydrateRoot(document, <RemixBrowser />)
+  })
+}).catch((error) => {
+  console.error('Failed to start API mocking:', error)
 })
+
